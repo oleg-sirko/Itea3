90ff2b16263bdb2a2c6eeb779adaaf223d1772b7
Abror2199 <avazov_abror21@mail.ru>
2022-02-08 23:25:32 +0500

    Commit changes

--- src/task1/ForCycle.java
+++ src/task1/ForCycle.java
@@ -3,7 +3,7 @@ package task1;
 public class ForCycle {
     public static void main(String args[]) {
 
-        int num = 3;
+        int num = 2;
 
         for (int i = 1; i <= num; i++) {
             System.out.println(i);
