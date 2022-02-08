2506c7682b314b86f8a2284beca74874aa061e2a
Abror2199 <avazov_abror21@mail.ru>
2022-02-09 00:07:00 +0500

    something

--- src/task1/ForCycle.java
+++ src/task1/ForCycle.java
@@ -3,7 +3,7 @@ package task1;
 public class ForCycle {
     public static void main(String args[]) {
 
-        int num = 10;
+        int num = 11;
 
         for (int i = 1; i <= num; i++) {
             System.out.println(i);
