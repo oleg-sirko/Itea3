606ca6c3d035ee866aab2ad79e30e8705928f4d7
Abror2199 <avazov_abror21@mail.ru>
2022-02-10 19:36:12 +0500

    Completed task3 by Abror

--- src/task3/execute/RunTask.java
+++ src/task3/execute/RunTask.java
@@ -1,10 +1,18 @@
 package task3.execute;
 
+import task3.classes.Student;
+
 public class RunTask {
-    static String name = ""; //provide your name as value to variable
+
+    public String runMethod() {
+        String name = "Abror";
+        return name;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(/* print result of runTask() method from Student class providing name as input
-        Expected output in Console im my case: "Oleg Sirko has scored 3 points for Task3"*/);
+        Student person1 = new Student();
+        String messageOut = person1.runTask("Abror");
+        System.out.println(messageOut);
     }
 }
