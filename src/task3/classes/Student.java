03908e982826528829203c314249d633012766d4
Anna Vysotskaya <vysotskaya.anya@gmail.com>
2022-02-10 10:00:29 -0500

    no message

--- src/task3/classes/Student.java
+++ src/task3/classes/Student.java
@@ -2,11 +2,13 @@ package task3.classes;
 
 public class Student {
 
-    public String runTask (String name) {
-        String result = name + " has scored 3 points for Task3";
+   /* public String runTask (String name) {
+
+    }*/
+
+
+    public String runTask() {
+        String result = " has scored 3 points for Task3";
         return result;
     }
-
-
-
 }
--- src/task3/execute/RunTask.java
+++ src/task3/execute/RunTask.java
@@ -1,10 +1,14 @@
 package task3.execute;
+import task3.classes.Student;
 
 public class RunTask {
-    static String name = ""; //provide your name as value to variable
+    static String name = "Anna Vysoyskya"; //provide your name as value to variable
+
 
     public static void main(String[] args) {
-        System.out.println(/* print result of runTask() method from Student class providing name as input
-        Expected output in Console im my case: "Oleg Sirko has scored 3 points for Task3"*/);
+        Student sourse = new Student();
+        String value = sourse.runTask();
+        System.out.println(name + value);/* print result of runTask() method from Student class providing name as input
+        Expected output in Console im my case: "Oleg Sirko has scored 3 points for Task3"*/;
     }
 }
