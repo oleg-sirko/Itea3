3a9ea62749f8d6d9cfc1850589a4198a99b497c7
Abror2199 <avazov_abror21@mail.ru>
2022-02-15 13:32:37 +0500

    Task4 and Task3 commited by Abror

--- src/task4/homework/Task4.java
+++ src/task4/homework/Task4.java
@@ -3,13 +3,9 @@ package task4.homework;
 public class Task4 {
 
     public static void main(String[] args) {
-        // Expected output in Console: <your name> has scored 2 points for Task 4
-        // add or update code after this line
-        String name = "";
-        Student student = new Student();
-
-
-        // end of edit area
+        String name = "Abror";
+        Student student = new Student(name);
+        student.resolve();
         student.printScore();
     }
 
