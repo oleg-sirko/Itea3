085574fa023cfab62c17122c8b2364f9338ffc70
DESKTOP-6VLJSIH\unmes <undermess@gmail.com>
2022-02-17 16:01:39 +0200

    Task4

--- src/task4/homework/Task4.java
+++ src/task4/homework/Task4.java
@@ -5,9 +5,9 @@ public class Task4 {
     public static void main(String[] args) {
         // Expected output in Console: <your name> has scored 2 points for Task 4
         // add or update code after this line
-        String name = "";
-        Student student = new Student();
-
+        String name = "Serhii Matvieiev";
+        Student student = new Student(name);
+        student.resolve();
 
         // end of edit area
         student.printScore();
